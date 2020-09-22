44ec50d2ff0a5b93677cd918081daa6c1b05bc90
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2020-09-22 13:02:17 +0100

    List DLLs

---
+++ ecmwflibs/tools/list-dlls.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import os
+import sys
+
+from dlldiag.common import ModuleHeader, WindowsApi
+
+VCPKG = "C:/vcpkg/installed/{}-windows/bin/{}"
+
+
+def scan_module(module, depth):
+    print(" " * depth, "SCANNING", module)
+    try:
+        header = ModuleHeader(module)
+    except FileNotFoundError:
+        print(" " * depth, "... not found")
+        return
+
+    cwd = os.path.dirname(module)
+    architecture = header.getArchitecture()
+    for dll in header.listAllImports():
+        if WindowsApi.loadModule(dll, cwd, architecture) != 0:
+            print(" " * depth, "ERROR cannot load", dll)
+            continue
+
+        scan_module((cwd + "/" + dll), depth + 3)
+        scan_module(VCPKG.format(architecture, dll), depth + 3)
+
+
+scan_module(sys.argv[1], 0)
