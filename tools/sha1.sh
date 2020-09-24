8d5ec3acea4b003433037c969cbb648be9975ef8
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2020-09-24 18:11:29 +0100

    Print SHA1

--- tools/sha1.sh
+++ tools/sha1.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-source make/VERSIONS.make
+here=$(dirname $0)
+
+source $here/../make/VERSIONS.make
 eccodes=$(git ls-remote $GIT_ECCODES $ECCODES_VERSION | awk '{print $1;}')
 magics=$(git ls-remote $GIT_MAGICS $MAGICS_VERSION | awk '{print $1;}')
 echo "::set-output name=eccodes:$eccodes"
