936ac77b36e14b9e533a3c80203b5809a5a218f2
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2020-09-24 18:45:21 +0100

    Change test projection

--- tools/sha1.sh
+++ tools/sha1.sh
@@ -5,5 +5,5 @@ here=$(dirname $0)
 source $here/../make/VERSIONS.make
 eccodes=$(git ls-remote $GIT_ECCODES $ECCODES_VERSION | awk '{print $1;}')
 magics=$(git ls-remote $GIT_MAGICS $MAGICS_VERSION | awk '{print $1;}')
-echo "::set-output name=eccodes:$eccodes"
-echo "::set-output name=magics:$magics"
+echo "::set-output name=eccodes::$eccodes"
+echo "::set-output name=magics::$magics"
