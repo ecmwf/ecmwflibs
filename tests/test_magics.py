76daca87af47750c1fdcc9cc8e7bec46b6636bb6
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2020-09-25 15:03:59 +0100

    Version bump

--- tests/test_magics.py
+++ tests/test_magics.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-import pytest
-
 from Magics import macro as magics
-from Magics.Magics import MagicsError
 
 
 def test_magics_plot():
@@ -28,36 +25,3 @@ def test_magics_plot():
     )
     coast = magics.mcoast()
     magics.plot(output, proj, data, contour, coast)
-
-
-def test_magics_exception():
-
-    with pytest.raises(MagicsError):
-
-        # Setting of the output file name
-        output = magics.output(
-            output_formats=["png"],
-            output_name_first_page_number="off",
-            output_name="magics2",
-        )
-
-        # Import the  data
-        data = magics.mgrib(
-            grib_input_file_name="data.grib",
-        )
-
-        proj = magics.mmap(
-            subpage_lower_left_longitude=0.0,
-            subpage_upper_right_longitude=360.0,
-            subpage_upper_right_latitude=90.0,
-            subpage_map_projection="polar_stereographic",
-            subpage_lower_left_latitude=-90.0,
-        )
-
-        # Apply an automatic styling
-        contour = magics.mcont(
-            contour_automatic_setting="ecmwf",
-        )
-        coast = magics.mcoast()
-
-        magics.plot(output, proj, data, contour, coast)
