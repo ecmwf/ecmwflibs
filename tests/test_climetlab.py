e05283e1891e8cb3cf31e0fd6a06a53eb3a6d007
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2020-10-23 22:36:52 +0100

    climetlab

--- tests/test_climetlab.py
+++ tests/test_climetlab.py
@@ -45,7 +45,7 @@ def test_climetlab_grib():
             grib_input_file_name="climetlab.grib",
         ),
         macro.mcont(
-            contour_automatic_setting="ecmwf",
+            contour_automatic_setting="climetlab",
             legend=False,
         ),
         macro.mcoast(
@@ -101,7 +101,7 @@ def test_climetlab_grib():
             grib_input_file_name="climetlab.grib",
         ),
         macro.mcont(
-            contour_automatic_setting="ecmwf",
+            contour_automatic_setting="climetlab",
             legend=False,
         ),
         macro.mcoast(
