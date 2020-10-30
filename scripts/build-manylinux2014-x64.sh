34f23053fe1aff0f2f5a82d99d932fd9dc136fd2
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2020-10-30 16:02:36 +0000

    Build

--- scripts/build-manylinux2014-x64.sh
+++ scripts/build-manylinux2014-x64.sh
@@ -25,6 +25,102 @@ pip3 install ninja auditwheel meson
 ln -s /opt/python/cp36-cp36m/bin/meson /usr/local/bin/meson
 ln -s /opt/python/cp36-cp36m/bin/ninja /usr/local/bin/ninja
 
+# Build netcdf without curl
+
+git clone --depth 1 $GIT_NETCDF src/netcdf
+mkdir -p build-other/netcdf
+cd $TOPDIR/build-other/netcdf
+
+cmake -GNinja \
+    $TOPDIR/src/netcdf \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    -DENABLE_DAP=0 \
+    -DCMAKE_INSTALL_PREFIX=$TOPDIR/install
+
+cd $TOPDIR
+cmake --build build-other/netcdf --target install
+
+# Pixman is needed by cairo
+
+git clone --depth 1 $GIT_PIXMAN src/pixman
+cd src/pixman
+./autogen.sh
+./configure --prefix=$TOPDIR/install
+
+cd $TOPDIR
+make -C src/pixman install
+
+# Build cairo
+
+git clone --depth 1 $GIT_CAIRO src/cairo
+cd src/cairo
+./autogen.sh
+./configure \
+    --disable-xlib \
+    --disable-xcb \
+    --disable-qt \
+    --disable-quartz \
+    --disable-gl \
+    --disable-gobject \
+    --prefix=$TOPDIR/install
+
+cd $TOPDIR
+make -C src/cairo install
+
+# Build harfbuzz needed by pango
+
+git clone --depth 1 $GIT_HARFBUZZ src/harfbuzz
+
+mkdir -p build-other/harfbuzz
+cd src/harfbuzz
+meson setup --prefix=$TOPDIR/install \
+    -Dintrospection=disabled \
+    -Dwrap_mode=nofallback \
+    $TOPDIR/build-other/harfbuzz
+
+cd $TOPDIR
+ninja -C build-other/harfbuzz install
+
+# Build fridibi needed by pango
+
+git clone --depth 1 $GIT_FRIBIDI src/fridibi
+
+mkdir -p build-other/fridibi
+cd src/fridibi
+meson setup --prefix=$TOPDIR/install \
+    -Dintrospection=disabled \
+    -Dwrap_mode=nofallback \
+    -Ddocs=false \
+    $TOPDIR/build-other/fridibi
+
+cd $TOPDIR
+ninja -C build-other/fridibi install
+
+# Build pango
+
+# Versions after 1.43.0 require versions of glib2 higher than
+# the one in the dockcross image
+
+# We undefine G_LOG_USE_STRUCTURED because otherwise we will have a
+# undefined symbol g_log_structured_standard() when running on recent
+# docker images with recent versions of glib
+git clone --branch 1.43.0 $GIT_PANGO src/pango
+# cd src/pango
+# git checkout 1.43.0
+
+sed 's/.*G_LOG_USE_STRUCTURED.*//' < src/pango/meson.build > src/pango/meson.build.patched
+cp src/pango/meson.build.patched src/pango/meson.build
+sed 's/.*G_LOG_USE_STRUCTURED.*//' < src/pango/pango/meson.build > src/pango/pango/meson.build.patched
+cp src/pango/pango/meson.build.patched src/pango/pango/meson.build
+
+mkdir -p build-other/pango
+cd src/pango
+meson setup --prefix=$TOPDIR/install \
+    -Dwrap_mode=nofallback
+    $TOPDIR/build-other/pango
+
+cd $TOPDIR
+ninja -C build-other/pango install
 
 # Build eccodes
 
