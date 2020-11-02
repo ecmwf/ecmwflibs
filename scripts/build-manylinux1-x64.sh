b8ff755aa3a75bf517174e2a21ae14d2ec53cb8c
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2020-11-02 09:18:51 +0000

    tcl2

--- scripts/build-manylinux1-x64.sh
+++ scripts/build-manylinux1-x64.sh
@@ -36,9 +36,9 @@ LD_LIBRARY_PATH=$TOPDIR/install/lib:$TOPDIR/install/lib64:$LD_LIBRARY_PATH
 
 ################################################################
 
-git clone --depth 1 https://github.com/tcltk/tcl.git
+git clone  https://github.com/tcltk/tcl.git
 cd tcl/unix
-git checkout 278e65611 # core-8-6-2
+git checkout core-8-6-2
 
 ./configure
 make
