281682e8839c5e8ef7169bf1fa39e38f0933907b
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2020-11-03 10:35:58 +0000

    Linux

--- scripts/wheel-linux.sh
+++ scripts/wheel-linux.sh
@@ -3,6 +3,10 @@ set -eaux
 
 version=$(echo $1| sed 's/\.//')
 
+TOPDIR=$(/bin/pwd)
+
+LD_LIBRARY_PATH=$TOPDIR/install/lib:$TOPDIR/install/lib64:$LD_LIBRARY_PATH
+
 rm -fr dist wheelhouse
 /opt/python/cp${version}-cp${version}*/bin/python3 setup.py bdist_wheel
 auditwheel repair dist/*.whl
