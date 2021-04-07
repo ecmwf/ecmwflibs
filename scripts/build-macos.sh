c59d8b30133895b24820f51d639eba2edc07b704
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2021-04-07 16:51:15 +0100

    arm

--- scripts/build-macos.sh
+++ scripts/build-macos.sh
@@ -53,6 +53,7 @@ $TOPDIR/src/ecbuild/bin/ecbuild \
     -DENABLE_BUILD_TOOLS=0 \
     -DENABLE_MEMFS=1 \
     -DENABLE_INSTALL_ECCODES_DEFINITIONS=0 \
+    -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" \
     -DENABLE_INSTALL_ECCODES_SAMPLES=0 \
     -DCMAKE_INSTALL_PREFIX=$TOPDIR/install \
     -DCMAKE_INSTALL_RPATH=$TOPDIR/install/lib
@@ -71,6 +72,7 @@ $TOPDIR/src/ecbuild/bin/ecbuild \
     -DENABLE_FORTRAN=0 \
     -DENABLE_BUILD_TOOLS=0 \
     -Deccodes_DIR=$TOPDIR/install/lib/cmake/eccodes \
+    -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" \
     -DCMAKE_INSTALL_PREFIX=$TOPDIR/install \
     -DCMAKE_INSTALL_RPATH=$TOPDIR/install/lib
 
