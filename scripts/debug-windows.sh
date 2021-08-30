9c2e926461f9c0c82b35528223611393e9065143
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2021-08-30 16:52:22 +0100

    Debug

--- scripts/debug-windows.sh
+++ scripts/debug-windows.sh
@@ -7,10 +7,10 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
-set -eaux
+# set -eaux
 
-vcpkg install pango
+# vcpkg install pango
 
-find /c/vcpkg -name cairo.h -print
+# find /c/vcpkg -name cairo.h -print
 
-find /c/vcpkg -name 'pkg-conf*' -print
+# find /c/vcpkg -name 'pkg-conf*' -print
