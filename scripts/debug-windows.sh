e86571f2d0fa6c26cd6ac6b7686dd1f747eebe59
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2021-12-13 07:19:51 +0000

    Debug windows

--- scripts/debug-windows.sh
+++ scripts/debug-windows.sh
@@ -7,36 +7,15 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
-for p in hdf5 expat netcdf-c[core,netcdf-4] pango sqlite3[core,tool]
+set -eaux
+
+for p in netcdf-c[core,netcdf-4,hdf5]
 do
     echo $p:$WINARCH-windows
     vcpkg install $p:$WINARCH-windows
-    n=$(echo $p | sed 's/\[.*//')
-    v=$(vcpkg list $n | awk '{print $2;}')
-    echo "vcpkg $n $v"
-    echo "vcpkg $n $v" >> versions
 done
 
-exit
-# set -eaux
-set -x
-/c/msys64/mingw32/bin/pkg-config.exe --version
-/c/msys64/mingw64/bin/pkg-config.exe --version
-/c/msys64/usr/bin/pkg-config.exe --version
-/c/rtools40/mingw32/bin/pkg-config.exe --version
-/c/rtools40/mingw64/bin/pkg-config.exe --version
-/c/rtools40/ucrt64/bin/pkg-config.exe --version
-/c/Strawberry/perl/bin/pkg-config --version
-/c/Strawberry/perl/bin/pkg-config.bat --version
-
-
-
-
-
-exit
-
-vcpkg install glib pango
-
-find /c/vcpkg -name cairo.h -print
-
-find /c/vcpkg -name glib-object.h -print
+cd tests
+cmake .
+make
+./open_netcdf test.nc4
