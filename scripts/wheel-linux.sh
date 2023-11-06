4a44df096bdf06c20b0d3e4793b70386ec4bf69f
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2023-11-06 09:25:39 +0000

    python3.6 via conda

--- scripts/wheel-linux.sh
+++ scripts/wheel-linux.sh
@@ -15,9 +15,18 @@ TOPDIR=$(/bin/pwd)
 
 LD_LIBRARY_PATH=$TOPDIR/install/lib:$TOPDIR/install/lib64:$LD_LIBRARY_PATH
 
+if [ "$version" -eq "3.6" ]
+then
+    conda config --set restore_free_channel True
+    conda create -n py36 python=3.6
+    conda activate py36
+    alias this_python=python3
+else
+    alias this_python=/opt/python/cp${version}-cp${version}*/bin/python3
+fi
 
 rm -fr dist wheelhouse
-/opt/python/cp${version}-cp${version}*/bin/python3 setup.py bdist_wheel
+this_python setup.py bdist_wheel
 
 # Do it twice to get the list of libraries
 
@@ -25,9 +34,9 @@ auditwheel repair dist/*.whl
 unzip -l wheelhouse/*.whl | grep 'ecmwflibs.libs/' > libs
 pip3 install -r tools/requirements.txt
 
-python3 ./tools/copy-licences.py libs
+this_python ./tools/copy-licences.py libs
 
 rm -fr dist wheelhouse
-/opt/python/cp${version}-cp${version}*/bin/python3 setup.py bdist_wheel
+this_python setup.py bdist_wheel
 auditwheel repair dist/*.whl
 rm -fr dist
