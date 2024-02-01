00de612e9a6f2ea04016aa6a155d408fca42cee3
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2024-02-01 09:38:02 -0500

    diet

--- scripts/wheel-macos.sh
+++ scripts/wheel-macos.sh
@@ -9,35 +9,42 @@
 
 set -eaux
 
-ARCH="arch -$(arch)"
+arch=$(arch)
+ARCH="arch -$arch"
+
+diet() {
+    # Remove the architectures we don't need
+
+    echo =================================================================
+
+    cd dist
+    name=$(ls -1 *.whl)
+    echo $name
+    unzip *.whl
+    ls -l
+    cd ecmwflibs
+    ls -l
+    so=$(ls -1 *.so)
+    echo "$so"
+    lipo -info $so
+    lipo -extract $(arch) $so -output $so.$(arch)
+    mv $so.$(arch) $so
+    lipo -info $so
+    cd ..
+    zip -r $name ecmwflibs
+    cd ..
+
+    echo =================================================================
+
+}
 
 # version=$(echo $1| sed 's/\.//')
 
 pip3 install wheel delocate
 
 rm -fr dist wheelhouse tmp
-$ARCH python3 setup.py bdist_wheel --plat-name $(arch)
-
-echo =================================================================
-
-cd dist
-name=$(ls -1 *.whl)
-echo $name
-unzip *.whl
-ls -l
-cd ecmwflibs
-ls -l
-so=$(ls -1 *.so)
-echo "$so"
-lipo -info $so
-lipo -extract $(arch) $so -output $so.$(arch)
-mv $so.$(arch) $so
-lipo -info $so
-cd ..
-zip -r $name ecmwflibs
-cd ..
-
-echo =================================================================
+$ARCH python3 setup.py bdist_wheel --plat-name $arch
+diet
 
 # Do it twice to get the list of libraries
 
@@ -47,5 +54,6 @@ pip3 install -r tools/requirements.txt
 python3 ./tools/copy-licences.py libs
 
 rm -fr dist wheelhouse
-python3 setup.py bdist_wheel
+$ARCH python3 setup.py bdist_wheel
+diet
 $ARCH delocate-wheel -w wheelhouse dist/*.whl
