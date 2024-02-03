90bc8fe291cffb6f165617068ec6ef329ed21696
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2024-02-03 20:22:29 +0000

    tests

--- scripts/select-python.sh
+++ scripts/select-python.sh
@@ -10,12 +10,12 @@ ls -l /opt/homebrew/opt/python@$version/libexec/bin
 
 # Looks like python3 is not a symlink to python in 3.11
 
-if [[ ! -f /opt/homebrew/opt/python@$version/libexec/bin/python3]]
+if [[ ! -f /opt/homebrew/opt/python@$version/libexec/bin/python3 ]]
 then
     ln -s /opt/homebrew/opt/python@$version/libexec/bin/python /opt/homebrew/opt/python@$version/libexec/bin/python3
 fi
 
-if [[ ! -f /opt/homebrew/opt/python@$version/libexec/bin/pip3]]
+if [[ ! -f /opt/homebrew/opt/python@$version/libexec/bin/pip3 ]]
 then
     ln -s /opt/homebrew/opt/python@$version/libexec/bin/pip /opt/homebrew/opt/python@$version/libexec/bin/pip3
 fi
