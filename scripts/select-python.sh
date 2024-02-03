e3a3a2295d09a2f22628341aee044929c4e15188
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2024-02-03 17:56:34 +0000

    add-path

--- scripts/select-python.sh
+++ scripts/select-python.sh
@@ -5,4 +5,7 @@ version=$1
 brew install python@$1
 # echo ::add-path::/opt/homebrew/opt/python@$VERSION/libexec/bin
 
-echo /opt/homebrew/opt/python@$VERSION/libexec/bin >> $GITHUB_PATH
+echo /opt/homebrew/opt/python@$version/libexec/bin >> $GITHUB_PATH
+
+echo $GITHUB_PATH
+cat $GITHUB_PATH
