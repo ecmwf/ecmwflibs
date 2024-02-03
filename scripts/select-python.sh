0c352bf975cf6e984a0294ffd5727d580b5eeeb8
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2024-02-03 20:19:16 +0000

    tests

--- scripts/select-python.sh
+++ scripts/select-python.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-set -x
+set -xe
 version=$1
 
 brew install python@$1
-# echo ::add-path::/opt/homebrew/opt/python@$VERSION/libexec/bin
 
 echo /opt/homebrew/opt/python@$version/libexec/bin >> $GITHUB_PATH
 
 ls -l /opt/homebrew/opt/python@$version/libexec/bin
+
+# Looks like python3 is not a symlink to python in 3.11
+
+if [[ ! -f /opt/homebrew/opt/python@$version/libexec/bin/python3]]
+then
+    ln -s /opt/homebrew/opt/python@$version/libexec/bin/python /opt/homebrew/opt/python@$version/libexec/bin/python3
+fi
+
+if [[ ! -f /opt/homebrew/opt/python@$version/libexec/bin/pip3]]
+then
+    ln -s /opt/homebrew/opt/python@$version/libexec/bin/pip /opt/homebrew/opt/python@$version/libexec/bin/pip3
+fi
+
+ls -l
